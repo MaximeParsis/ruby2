70a66eec18a7e7ed7d8fca6b33df9098adc1d98b
Maxime Parsis <maximeprss@gmail.com>
2018-12-28 16:48:49 +0100

    exo7a

---
+++ exo7a.rb
@@ -0,0 +1,3 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp # gets chomp permet de renseigner le terminal quil va y avoir une saisie manuelle , associé a user_name
+puts user_name
