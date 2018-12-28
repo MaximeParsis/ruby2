1bf81824e942098418c0135fb7b19980e54842b7
Maxime Parsis <maximeprss@gmail.com>
2018-12-28 17:09:30 +0100

    exo9

---
+++ exo9.rb
@@ -0,0 +1,6 @@
+user_name = gets.chomp
+user_lastename = gets.chomp
+puts "hello" + user_name  + user_lastename
+
+# aprés 15m de tentative infructueuse a mettre des espaces, je passe au prochain exercice par manque de patiente. Ne men voulez pas svp
+
