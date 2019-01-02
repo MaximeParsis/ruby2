81abb1ba0bf008b1294702b7721912b7d9414322
Maxime Parsis <maximeprss@gmail.com>
2019-01-02 16:29:33 +0100

    exo20

---
+++ exo20.rb
@@ -0,0 +1,14 @@
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+
+x = gets.chomp.to_i 
+y = 1
+
+puts "Voici la pyramide:"
+while y < x
+    x.times {
+    puts "#" * y
+    y += 1
+}
+
+
+end
